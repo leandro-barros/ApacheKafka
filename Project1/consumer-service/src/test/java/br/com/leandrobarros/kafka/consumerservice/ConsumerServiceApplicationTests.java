0c7045d7dc308b8837c0edbc5bfc898d03dc426e
Leandro Barros <leandroedbarros@gmail.com>
2022-03-20 20:15:05 -0300

    Creating project consumer-service

---
+++ Project1/consumer-service/src/test/java/br/com/leandrobarros/kafka/consumerservice/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrobarros.kafka.consumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
