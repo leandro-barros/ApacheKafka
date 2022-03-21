c8c1e22d228882bca6c3e5c28f9e6374fd4c96ec
Leandro Barros <leandroedbarros@gmail.com>
2022-03-20 21:18:38 -0300

    Implementing controller in producer

---
+++ Project1/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/controller/MessageController.java
@@ -0,0 +1,21 @@
+package br.com.leandrobarros.kafka.producerservice.controller;
+
+import br.com.leandrobarros.kafka.producerservice.service.MessageService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("publish-message")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    public ResponseEntity<String> publishMessage(@RequestBody String message) {
+        messageService.setMessageTopicProducer(message);
+        return ResponseEntity.ok().body("Mensagem enviada com sucesso: " + message);
+    }
+}
