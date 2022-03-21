3bb5008f05cb61c1145940ce9715ce74f95ae89b
Leandro Barros <leandroedbarros@gmail.com>
2022-03-21 20:33:23 -0300

    Implementing consumer message

--- Project1/consumer-service/src/main/java/br/com/leandrobarros/kafka/consumerservice/listener/ConsumerListener.java
+++ Project1/consumer-service/src/main/java/br/com/leandrobarros/kafka/consumerservice/listener/ConsumerListener.java
@@ -1,6 +1,16 @@
 package br.com.leandrobarros.kafka.consumerservice.listener;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class ConsumerListener {
 
+    @KafkaListener(topics = "${topic.topic-practice}", groupId = "group_id")
+    public void consumerMessage(String message) {
+        log.info("--> Consumed message: " + message);
+    }
 
 }
