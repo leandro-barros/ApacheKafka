8b91598ecaed54f3bbcd1c71a8502f66979ece68
Leandro Barros <leandroedbarros@gmail.com>
2022-03-21 19:44:32 -0300

    Adjustement

--- Project1/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/controller/MessageController.java
+++ Project1/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/controller/MessageController.java
@@ -3,6 +3,7 @@ package br.com.leandrobarros.kafka.producerservice.controller;
 import br.com.leandrobarros.kafka.producerservice.service.MessageService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class MessageController {
 
     private final MessageService messageService;
 
+    @PostMapping
     public ResponseEntity<String> publishMessage(@RequestBody String message) {
         messageService.setMessageTopicProducer(message);
         return ResponseEntity.ok().body("Mensagem enviada com sucesso: " + message);
--- Project1/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/service/MessageService.java
+++ Project1/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/service/MessageService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageService {
 
-    @Value("${topic.producer")
+    @Value("${topic.topic-producer}")
     private String topicProducer;
 
     @Autowired
