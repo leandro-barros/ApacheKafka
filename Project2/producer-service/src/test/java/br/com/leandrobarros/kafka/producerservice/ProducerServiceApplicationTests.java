2d40e6ac27e8f6cc81c3d4e558bc98a20383b801
Leandro Barros <leandroedbarros@gmail.com>
2022-03-24 23:15:55 -0300

    Creating project2 with producer

---
+++ Project2/producer-service/src/test/java/br/com/leandrobarros/kafka/producerservice/ProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrobarros.kafka.producerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
