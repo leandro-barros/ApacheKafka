e55b263d717a888ded672993c99f0d63555a1e0d
Leandro Barros <leandroedbarros@gmail.com>
2022-03-27 10:10:57 -0300

    Implementing producer class

--- Project2/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/producer/CarProducer.java
+++ Project2/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/producer/CarProducer.java
@@ -1,4 +1,27 @@
 package br.com.leandrobarros.kafka.producerservice.producer;
 
+import br.com.leandrobarros.kafka.producerservice.dto.CarDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Slf4j
+@Service
 public class CarProducer {
+
+    @Value("${topic.name}")
+    private String topicName;
+
+    private final KafkaTemplate<String, CarDto> kafkaTemplate;
+
+    public void sendMessage(CarDto carDto) {
+        kafkaTemplate.send(topicName, carDto).addCallback(
+                success -> log.info("Message send " + success.getProducerRecord().value()),
+                failure -> log.error("Message failure " + failure.getMessage())
+        );
+    }
+
 }
