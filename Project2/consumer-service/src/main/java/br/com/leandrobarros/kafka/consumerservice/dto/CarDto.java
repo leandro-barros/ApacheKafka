4ed429ce58756d6f9bd5033035326445e342113e
Leandro Barros <leandroedbarros@gmail.com>
2022-03-27 20:42:54 -0300

    Creating DTO in car

---
+++ Project2/consumer-service/src/main/java/br/com/leandrobarros/kafka/consumerservice/dto/CarDto.java
@@ -0,0 +1,14 @@
+package br.com.leandrobarros.kafka.consumerservice.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CarDto {
+
+    private String id;
+
+    private String model;
+
+    private String color;
+
+}
