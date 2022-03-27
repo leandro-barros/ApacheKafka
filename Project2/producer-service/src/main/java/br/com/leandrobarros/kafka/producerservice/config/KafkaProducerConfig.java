53fd3c637af06e1e851fc3ede8fecc35aa83b2ba
Leandro Barros <leandroedbarros@gmail.com>
2022-03-27 11:21:30 -0300

    Implementing config class in Kafka

---
+++ Project2/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/config/KafkaProducerConfig.java
@@ -0,0 +1,46 @@
+package br.com.leandrobarros.kafka.producerservice.config;
+
+import br.com.leandrobarros.kafka.producerservice.dto.CarDto;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+    @Value("${topic.name}")
+    private String topicName;
+
+    @Bean
+    public NewTopic createTopic() {
+        return new NewTopic(topicName, 3, (short) 1);
+    }
+
+    @Bean
+    public ProducerFactory<String, CarDto> carProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, CarDto> kafkaTemplate() {
+        return new KafkaTemplate<>(carProducerFactory());
+    }
+}
--- Project2/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/dto/CarDto.java
+++ Project2/producer-service/src/main/java/br/com/leandrobarros/kafka/producerservice/dto/CarDto.java
@@ -1,9 +1,11 @@
 package br.com.leandrobarros.kafka.producerservice.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.Setter;
 
 @Setter
+@Getter
 @Builder
 public class CarDto {
 
