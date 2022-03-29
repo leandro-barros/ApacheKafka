257e5c6d3adc2ce4b5b005a9e232da49f9a2667e
Leandro Barros <leandroedbarros@gmail.com>
2022-03-28 22:09:34 -0300

    Settings listener of consumer

--- Project2/consumer-service/src/main/java/br/com/leandrobarros/kafka/consumerservice/consumer/CarConsumer.java
+++ Project2/consumer-service/src/main/java/br/com/leandrobarros/kafka/consumerservice/consumer/CarConsumer.java
@@ -1,6 +1,8 @@
 package br.com.leandrobarros.kafka.consumerservice.consumer;
 
+import br.com.leandrobarros.kafka.consumerservice.dto.CarDto;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -16,7 +18,8 @@ public class CarConsumer {
     private String groupId;
 
     @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.group-id}", containerFactory = "carKafkaListenerContainerFactory")
-    public void listenTopicCar() {
-
+    public void listenTopicCar(ConsumerRecord<String, CarDto> consumerRecord) {
+        log.info("Received message " + consumerRecord.partition());
+        log.info("Received message: " + consumerRecord.value());
     }
 }
