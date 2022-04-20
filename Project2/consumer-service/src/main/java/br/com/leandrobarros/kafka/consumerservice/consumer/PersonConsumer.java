a3e19c8592caf59cb49589076e25bde7cdb7efa1
Leandro Barros <leandroedbarros@gmail.com>
2022-04-19 22:36:39 -0300

    Consumer partition specific

--- Project2/consumer-service/src/main/java/br/com/leandrobarros/kafka/consumerservice/consumer/PersonConsumer.java
+++ Project2/consumer-service/src/main/java/br/com/leandrobarros/kafka/consumerservice/consumer/PersonConsumer.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,16 @@ public class PersonConsumer {
     public void historyPerson(PersonDto personDto) {
         log.info("Thread: {}", Thread.currentThread().getId());
         log.info("History: {} ", personDto);
+    }
+
+    @KafkaListener(topicPartitions = {@TopicPartition(topic = "my-topic", partitions = "0")}, groupId = "my-group")
+    public void listenerMyTopic(ConsumerRecord<String, PersonDto> consumerRecord) {
+        log.info("Consumer Partition specific: {} | message: {} ", consumerRecord.partition(), consumerRecord.value());
+    }
+
+    @KafkaListener(topicPartitions = {@TopicPartition(topic = "my-topic", partitions = "1-9")}, groupId = "my-group")
+    public void listenerMyTopicParttion1a9(ConsumerRecord<String, PersonDto> consumerRecord) {
+        log.info("Consumer Partition 1-9: {} | message: {} ", consumerRecord.partition(), consumerRecord.value());
     }
 
 }
